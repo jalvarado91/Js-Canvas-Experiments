2baac112f12641b1e72263542a1d62c109b99383
Juan Alvarado <jalvarado91@gmail.com>
2013-08-19 10:37:40 -0400

    Define Vector Class and vector methods

--- Particles/js/main.js
+++ Particles/js/main.js
@@ -4,6 +4,27 @@ var ctx = canvas.getContext('2d');
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
 
+function Vector(x, y) {
+  this.x = x || 0;
+  this.y = y || 0;
+}
+
+Vector.prototype.add = function(v) {
+  this.x += v.x;
+  this.y += v.y;
+};
+
+Vector.prototype.getMagnitude = function() {
+  return( Math.sqrt((this.x*this.x) + (this.y*this.y)) );
+};
+
+Vector.prototype.getAngle = function() {
+  return Math.atan2(this.x, this.y);
+};
+
+Vector.fromAngle = function(angle, magnitute) {
+  return new Vector(magnitute*Math.cos(angle), magnitute*Math.sin(angle));
+}
 
 function loop() {
   clear();
@@ -21,7 +42,7 @@ function update() {
 }
 
 function draw() {
-  
+
 }
 
 function queue() {
