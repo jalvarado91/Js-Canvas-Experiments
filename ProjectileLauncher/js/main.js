ff34413610ab4b13fb2a5fe2d9ba02c144effb2d
Juan Alvarado <jalvarado91@gmail.com>
2013-09-13 18:13:53 -0400

    begin to stub gravity

--- ProjectileLauncher/js/main.js
+++ ProjectileLauncher/js/main.js
@@ -9,9 +9,14 @@ canvas.width = window.innerWidth;
 var midX = canvas.width/2;
 var midY = canvas.height/2;
 
+var particleNum = 10000;
 var emissionRate = 10;
 var particleSize = 1.7;
-
+var objectSize = 3; // drawSize of emitter/field
+var maxMass = 1500;
+var bigG = 0.0001;
+var g = -9.8;  //Little g := 9.8 m/s^2
+var coloredParticles = false;
 
 /* Vector Class
  ********************/
@@ -47,12 +52,12 @@ function Particle (point, velocity, acceleration) {
   this.acceleration = acceleration || new Vector(0,0);
   this.color = 'rgb(0, 0, 255)';
 }
-Particle.prototype.calculateForces = function(fields) {
+Particle.prototype.calculateForces = function(objects) {
   var totalAccelerationX = 0;
   var totalAccelerationY = 0;
 
-  for (var i = 0; i < fields.length; i++) {
-    var field = fields[i];
+  for (var i = 0; i < objects.length; i++) {
+    var field = objects[i];
     
     var diff_x = field.position.x - this.position.x;
     var diff_y = field.position.y - this.position.y;
@@ -62,6 +67,8 @@ Particle.prototype.calculateForces = function(fields) {
     totalAccelerationX += diff_x*force;
     totalAccelerationY += diff_y*force;
   };
+
+  totalAccelerationY += g;
 
   this.acceleration = new Vector(totalAccelerationX, totalAccelerationY);
 };
@@ -116,16 +123,102 @@ Field.prototype.setMass = function(mass) {
 /* Updating Functions
  ********************/
 
+function addParticles () {
+  if (particles.length > particleNum) {return};
 
+  for (var i = 0; i < emitters.length; i++) {
+
+    for (var j = 0; j < emissionRate; j++) {
+      particles.push(emitters[i].emitParticle());
+    };
+
+  };
+}
+
+function plotParticles (boundsX, boundsY) {
+  // Hold particles within bounds
+  var currentParticles = [];
+
+  for (var i = 0; i < particles.length; i++) {
+    var particle = particles[i];
+    
+    var x = particle.position.x;
+    var y = particle.position.y;
+
+    if(x < 0 || x > boundsX || y < 0 || y > boundsY) continue;
+
+    // Calculate the forces that will affect acceleration and therefore velocity before moving particle
+    particle.calculateForces(fields);
+    particle.move();
+
+    // add this particle to current particles
+    currentParticles.push(particle);
+  }
+
+  // Update global particles
+  particles = currentParticles;
+}
 
 /* Drawing Functions
  ********************/
+function drawParticles () {
+  for (var i = 0; i < particles.length; i++) {
+    particles[i].draw();
+  };
+}
 
+/* Misc Utilities */
+function drawCircle(object) {
+  ctx.fillStyle = object.drawColor;
+  ctx.beginPath();
+  ctx.arc(object.position.x, object.position.y, objectSize, 0, Math.PI * 2);
+  ctx.closePath();
+  ctx.fill();
+}
+
+function addField() {
+  // new Field(position, mass);
+  var positionX = Math.random() * canvas.width;
+  var positionY = Math.random() * canvas.height;
+  var position = new Vector(positionX, positionY);
+  var mass = (Math.random() * maxMass*2) - maxMass;
+  var dwarf = new Field(position, mass);
+  
+  fields.push(dwarf);
+}
+
+function removeField() {
+  var lastField = fields.length - 1;
+  fields.splice(lastField, 1);
+}
+
+function clearFields() {
+  fields = [];
+}
+
+function toggleColors() {
+  if(coloredParticles) { 
+    coloredParticles = false;
+    colorsButton.innerHTML = "Colors!";
+  }
+  else {
+    coloredParticles = true;
+    colorsButton.innerHTML = "No Colors!";
+  }
+}
 
 /* Variables that use classes defined 
  * above placed here due to hoisting 
  *************************************/
+var particles = [];
 
+var emitters = [
+  new Emitter(new Vector(midX - 150, midY), Vector.fromAngle(6, 2), Math.PI)
+];
+ 
+var fields = [
+
+];
 
 
 /* Loop Sequence */ 
@@ -141,11 +234,14 @@ function clear() {
 }
 
 function update() {
-  // plotParticles(canvas.width, canvas.height);
+  addParticles();
+  plotParticles(canvas.width, canvas.height);
 }
 
 function draw() {
-
+  drawParticles();
+  fields.forEach(drawCircle);
+  emitters.forEach(drawCircle);
 }
 
 function queue() {
